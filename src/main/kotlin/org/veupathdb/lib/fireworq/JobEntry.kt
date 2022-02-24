dc343d1b3c86c7f5cca3e95908bda2e92f85c052
Elizabeth <foxcapade@gmail.com>
2022-02-24 09:20:05 -0500

    add missing properties

--- src/main/kotlin/org/veupathdb/lib/fireworq/JobEntry.kt
+++ src/main/kotlin/org/veupathdb/lib/fireworq/JobEntry.kt
@@ -1,12 +1,14 @@
 package org.veupathdb.lib.fireworq
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.JsonNode
 import java.time.LocalDateTime
 
 data class JobEntry(
   @JsonProperty("id")          val jobID:      Int,
   @JsonProperty("category")    val category:   String,
   @JsonProperty("url")         val url:        String,
+  @JsonProperty("payload")     val payload:    JsonNode,
   @JsonProperty("status")      val status:     String,
   @JsonProperty("created_at")  val createdAt:  LocalDateTime,
   @JsonProperty("next_try")    val nextTry:    LocalDateTime,
